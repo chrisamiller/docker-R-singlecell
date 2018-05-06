e869063ae4afc9ad7e72a91ce3c6666979a25912
Chris Miller <chrisamiller@gmail.com>
2018-05-06 11:19:33 -0500

    fixing droplet utils

--- r-dropletutils.R
+++ r-dropletutils.R
@@ -1,2 +1,2 @@
 source("https://bioconductor.org/biocLite.R")
-install.packages(c("tidyverse","ggplot2","Hmisc","plotrix","png"),repo=paste0("https://mran.microsoft.com/snapshot/",format(Sys.Date(), format="%Y-%m-%d")))
+biocLite(c("DropletUtils"))
